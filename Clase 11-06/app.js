9e66ced50cdc531006606441e6bb186697a6412c
Laureano Orr <laureano.orr@hotmail.com>
2024-06-11 20:17:56 -0300

    Agregar ejercicio con while y suma

--- Clase 11-06/app.js
+++ Clase 11-06/app.js
@@ -1,3 +1,4 @@
+/*
 let num = prompt("Ingrese el número.");
 let num2 = prompt("Ingrese el siguiente número.")
 
@@ -6,7 +7,7 @@ let suma = num + num2;
 alert(suma)
 
 
-/*
+
 if(num > 1000)
 {
 alert("El número que ingresaste es mayor a 1000.");
@@ -24,3 +25,19 @@ else
     alert("No pusiste nada.")
 }
 */
+
+let num, suma, aux, resultado;
+resultado = 0;
+while(num != "SALIR" || num != "salir")
+    {
+        num = parseInt(prompt("Ingresa un número. Escriba SALIR para salir del ciclo."));
+        if(num =="salir" || num =="SALIR")
+        {
+            break;
+        }
+        
+    
+        resultado += num;
+        alert(resultado);
+          
+    }
