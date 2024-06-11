88770ae99f9ee565054064d2d7ff7a16e7751268
Laureano Orr <laureano.orr@hotmail.com>
2024-06-11 19:24:56 -0300

    Clase 11-06
    Condicionales y estructuras de repetición

---
+++ Clase 11-06/app.js
@@ -0,0 +1,26 @@
+let num = prompt("Ingrese el número.");
+let num2 = prompt("Ingrese el siguiente número.")
+
+let suma = num + num2;
+
+alert(suma)
+
+
+/*
+if(num > 1000)
+{
+alert("El número que ingresaste es mayor a 1000.");
+}
+else if (num == "Hola")
+{
+    alert("Ingresaste hola, no un número.");
+}
+else if(num >= 10 && num <= 50)
+{
+    alert("El número esta en el rango de 10 y 50.");
+}
+else
+{
+    alert("No pusiste nada.")
+}
+*/
